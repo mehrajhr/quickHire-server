cb88cd175a896bf897d1574ee8cab6c47f81073b
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-03-01 05:32:51 +0600

    final and deploy to vercel

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("quickhire");
     const jobsCollection = db.collection("jobs");
@@ -80,7 +80,7 @@ async function run() {
         if (!user || user.role !== "admin") {
           return res.status(403).send({ message: "Access Denied" });
         }
-        
+
         const result = await jobsCollection.deleteOne({
           _id: new ObjectId(id),
         });
@@ -133,10 +133,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!",
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
